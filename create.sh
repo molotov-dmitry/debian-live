ce9e8fc5907a0336308fa1c4161ba6031052e3d8
Dmitry Sorokin <molotov.dmitry@gmail.com>
2019-02-07 19:20:32 +0300

    Updated config script.

--- create.sh
+++ create.sh
@@ -4,21 +4,21 @@ ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
 cd "${ROOT_PATH}" || exit 1
 
 lb config \
-	--distribution testing \
+	--distribution buster \
 	--architecture amd64 \
 	--debian-installer live \
 	--binary-images iso-hybrid \
 	--debconf-frontend dialog \
 	--chroot-filesystem squashfs \
-	--parent-mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
-	--parent-mirror-binary 'http://mirror.yandex.ru/debian/' \
-	--mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
-	--mirror-binary 'http://mirror.yandex.ru/debian/' \
-	--archive-areas "main non-free contrib" \
 	--apt-options "--force-yes --yes" \
 	--mode debian \
 	--system live \
-	--firmware-binary true
+	--firmware-binary true \
+	--archive-areas "main non-free contrib"
+#	--parent-mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
+#	--parent-mirror-binary 'http://mirror.yandex.ru/debian/' \
+#	--mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
+#	--mirror-binary 'http://mirror.yandex.ru/debian/'
 
 mkdir -p config/package-lists
 
