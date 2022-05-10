480b7ee32654bcc4e23d9bb61fcf230a8a504947
Dmitry Sorokin <molotov.dmitry@gmail.com>
2022-05-10 18:42:34 +0300

    Updated create script.

--- create.sh
+++ create.sh
@@ -3,8 +3,12 @@
 ROOT_PATH="$(cd "$(dirname "$0")" && pwd)"
 cd "${ROOT_PATH}" || exit 1
 
+set -e
+
+lb clean --all
+
 lb config \
-	--distribution buster \
+	--distribution testing \
 	--architecture amd64 \
 	--debian-installer live \
 	--binary-images iso-hybrid \
@@ -14,13 +18,19 @@ lb config \
 	--mode debian \
 	--system live \
 	--firmware-binary true \
-	--archive-areas "main non-free contrib"
-#	--parent-mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
-#	--parent-mirror-binary 'http://mirror.yandex.ru/debian/' \
-#	--mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
-#	--mirror-binary 'http://mirror.yandex.ru/debian/'
+        --parent-debian-installer-distribution daily \
+	--archive-areas "main non-free contrib" \
+	--parent-mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
+	--parent-mirror-binary 'http://mirror.yandex.ru/debian/' \
+	--mirror-bootstrap 'http://mirror.yandex.ru/debian/' \
+	--mirror-binary 'http://mirror.yandex.ru/debian/'
 
 mkdir -p config/package-lists
 
-echo "task-gnome-desktop" > config/package-lists/desktop.list.chroot
-echo "debian-installer-launcher" > config/package-lists/installer.list.chroot
+echo 'linux-headers-generic firmware-linux-free amd64-microcode firmware-iwlwifi firmware-linux firmware-linux-nonfree firmware-misc-nonfree firmware-sof-signed intel-microcode' > config/package-lists/kernel.list.chroot
+echo 'grub-common grub2-common grub-pc grub-pc-bin grub-efi-ia32-bin grub-efi-amd64-bin' > config/package-lists/boot.list.chroot
+echo 'task-gnome-desktop task-laptop laptop-mode-tools cups foomatic-db-compressed-ppds rsync' > config/package-lists/core.list.chroot
+
+lb build
+
+mv -f live-image-amd64.hybrid.iso /media/documents/Distrib/OS/debian-testing-gnome-amd64.iso
